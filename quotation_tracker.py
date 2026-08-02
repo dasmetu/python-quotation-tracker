d35aa167e25021e5656bb6822da44e7605eaa5dc
dasmetu <metehan.onbas@gmail.com>
2026-08-03 08:18:53 +0900

    Refactor quotation_tracker.py for improved structure

--- quotation_tracker.py
+++ quotation_tracker.py
@@ -1,3 +1,8 @@
+import json
+
+with open("/content/drive/MyDrive/Quotation Tracker/quotations.json","r") as quotation_file:
+  quotation_tracker = json.load(quotation_file)
+
 # =========================
 # Input Validation
 # =========================
@@ -25,7 +30,7 @@ def check_for_valid_string(prompt,choices):
     if user_input in choices:
       return user_input
     print("Please enter a valid choice")
-     
+
 
 # =========================
 # Repeat Check
@@ -50,7 +55,7 @@ def add_quotation():
   quotation_description = input("Please describe the quoation: ")
   quotation = {
       "customer":customer_name,
-      "quotation-number":"T-"+quotation_number,
+      "quotation-number": quotation_number,
       "price":quotation_price,
       "description":quotation_description
       }
@@ -75,14 +80,17 @@ def search_quotations(quotations):
       found = True
   if found == False:
     print("Quotation not found")
-    
+
 def quote_deletion(quotations):
   found = False
   quotation_number = input("Please enter the quotation number: ")
   for quote in quotations:
     if quote["quotation-number"] == quotation_number:
       display_quotations([quote])
-      user_input = check_for_valid_string("Do you really want to delete this quote: ",["Y","N","YES","NO"])
+      user_input = check_for_valid_string(
+          "Do you really want to delete this quote: ",
+          ["Y","N","YES","NO"]
+      )
       if user_input == "YES" or user_input == "Y":
         quotations.remove(quote)
         print("Quotation deleted")
@@ -91,7 +99,7 @@ def quote_deletion(quotations):
       found = True
   if found == False:
     print("Quotation not found")
-  
+
 def quote_update(quotations):
   found = False
   quotation_number = input("Please enter the quotation number: ")
@@ -104,31 +112,42 @@ def quote_update(quotations):
       print("4 - Quotation Description")
       print("5 - Cancel")
       found = True
+
       choice = check_for_range("Please choose an action: ",1,5)
+
       if choice == 1:
         customer_name = input("Please enter the new customer name: ")
         quote['customer'] = customer_name
+
       elif choice == 2:
         quotation_number = input("Please enter the new quotation number: ")
         quote['quotation-number'] = quotation_number
+
       elif choice == 3:
         price = check_for_integer("Please enter the new quotation price: ")
         quote['price'] = price
+
       elif choice == 4:
         description = input("Please enter the new quotation description: ")
         quote['description'] = description
+
       elif choice == 5:
         print('Action Cancelled')
+
   if found == False:
     print("Quotation not found")
+
+def save_quotations(quotations):
+  with open("/content/drive/MyDrive/Quotation Tracker/quotations.json", "w") as quotation_file:
+    json.dump(quotations, quotation_file, indent=4)
 
 # =========================
 # Main Function
 # =========================
 
-def main():
-  quotations = []
+def main(quotations):
   repeat_checker = True
+
   while repeat_checker == True:
     print("1 - Add Quotation")
     print("2 - Display Quotations")
@@ -136,25 +155,31 @@ def main():
     print("4 - Update Quotation")
     print("5 - Delete Quotation")
     print("6 - Exit Program")
+
     choice = check_for_range("Please choose an action: ",1,6)
+
     if choice == 1:
       quotations.append(add_quotation())
-      
+      save_quotations(quotations)
+
     elif choice == 2:
       display_quotations(quotations)
-      
+
     elif choice == 3:
       search_quotations(quotations)
 
     elif choice == 4:
       quote_update(quotations)
+      save_quotations(quotations)
 
     elif choice == 5:
       quote_deletion(quotations)
+      save_quotations(quotations)
 
     elif choice == 6:
       print("Goodbye")
       break
+
     repeat_checker = repeat_check("Do you want to do another operation(Y/N):")
 
-main()
+main(quotation_tracker)
