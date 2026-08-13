a0d57e93322769defd69fb2bf4f6cf6f4c067a23
dasmetu <metehan.onbas@gmail.com>
2026-08-13 11:56:19 +0900

    Refactor file path handling in storage.py

--- storage.py
+++ storage.py
@@ -1,8 +1,11 @@
 import json
 
+FILEPATH = "quotations.json"
+
 def load_quotations():
-  with open("/content/drive/MyDrive/Quotation Tracker/quotations.json","r") as quotation_file:
-    return json.load(quotation_file)
+    with open(FILEPATH, "r") as quotation_file:
+        return json.load(quotation_file)
+
 def save_quotations(quotations):
-  with open("/content/drive/MyDrive/Quotation Tracker/quotations.json", "w") as quotation_file:
-    json.dump(quotations, quotation_file, indent=4)
+    with open(FILEPATH, "w") as quotation_file:
+        json.dump(quotations, quotation_file, indent=4)
